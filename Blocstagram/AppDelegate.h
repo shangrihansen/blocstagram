bd519f37ae1592ecacb692ff9fdb05f45272b176
Colt Hansen <shangrihansen@gmail.com>
2014-10-28 13:03:09 -0700

    Initial Commit

---
+++ Blocstagram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Blocstagram
+//
+//  Created by Colt Hansen on 10/28/14.
+//  Copyright (c) 2014 Colt Hansen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
