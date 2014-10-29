8de41e5ef64caeb84057e2b16d736077f0419e9b
Colt Hansen <shangrihansen@gmail.com>
2014-10-28 17:11:56 -0700

    table view controller implemented

---
+++ Blocstagram/ImagesTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ImagesTableViewController.h
+//  Blocstagram
+//
+//  Created by Colt Hansen on 10/28/14.
+//  Copyright (c) 2014 Colt Hansen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImagesTableViewController : UITableViewController
+
+@end
